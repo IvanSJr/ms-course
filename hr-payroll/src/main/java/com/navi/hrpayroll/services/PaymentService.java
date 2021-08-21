4aec24908ccb0988cbfaeebcd5e143362a843bd9
IvanSJr <ivan.junior2706@gmail.com>
2021-08-20 21:03:41 -0300

    Utilizando o Feign Client para o integração de dois projetos.

--- hr-payroll/src/main/java/com/navi/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/navi/hrpayroll/services/PaymentService.java
@@ -1,31 +1,22 @@
 package com.navi.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.navi.hrpayroll.entities.Payment;
 import com.navi.hrpayroll.entities.Worker;
+import com.navi.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
 	
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	private WorkerFeignClient workFeignClient;
 	
 	public Payment getPayment(Long workerid, Integer dias) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerid);
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workFeignClient.findById(workerid).getBody();	
 		return new Payment(worker.getNome(), worker.getValorPorDia(), dias);
 	}
 }
